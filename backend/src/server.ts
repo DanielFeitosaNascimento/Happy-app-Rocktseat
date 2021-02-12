270d777e61a3f70602ca696cd3c1fe5927fd3786
DanielFeitosaNascimento <danfeitosan@hotmail.com>
2021-02-12 19:59:41 -0300

    termino da api happy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,39 +1,18 @@
 import express from 'express';
+import path from 'path';
+
+import 'express-async-errors';
+
 import './database/connection';
-import { getRepository } from 'typeorm';
-import Orphanage from './models/orphanege';
+import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 
 app.use(express.json());
-
-app.post('/orphanages', async (req, res) => {
-  const {
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    opening_on_weekends,
-  } = req.body;
-
-  const orphanagesRepository = getRepository(Orphanage);
-
-  const orphanage = orphanagesRepository.create({
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    opening_on_weekends,
-  });
-
-  await orphanagesRepository.save(orphanage);
-
-  return res.status(201).json(orphanage);
-});
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 app.listen(3333);
 
